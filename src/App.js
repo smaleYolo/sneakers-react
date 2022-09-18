9d765fa30a1aa55c2a7ec7992cf57b8777dabca2
smaleYolo <ilia.artamonov@yandex.ru>
2022-09-18 21:17:18 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Card from "./components/Card";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper clear">
+
+            <header className="d-flex justify-between align-center p-40">
+                <div className="d-flex align-center">
+                    <img src="/img/logo.png" width={40} height={40} alt="logo"/>
+                    <div>
+                        <h3 className="text-uppercase">React Sneakers</h3>
+                        <p className="opacity-5">Магазин лучших кроссовок</p>
+                    </div>
+                </div>
+                <ul className="d-flex">
+                    <li className="mr-20">
+                        <img src="/img/cart.svg" width={18} height={18} alt="cart"/>
+                        <span>1205 руб.</span>
+                    </li>
+                    <li className="mr-10">
+                        <img src="/img/heart.svg" width={18} height={18} alt="heart"/>
+                    </li>
+                    <li >
+                        <img src="/img/user.svg" width={18} height={18} alt="user"/>
+                    </li>
+                </ul>
+            </header>
+
+            <div className="content p-40">
+                <h1 className="mb-30">Все кроссовки</h1>
+                <div className="d-flex">
+
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className="card">
+            <img width={133} height={112} src="/img/sneakers/1.jpeg" alt=""/>
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column">
+                    <span>Цена:</span>
+                    <b>12 999 руб.</b>
+                </div>
+                <button className="button">
+                    <img width={11} height={11} src="/img/plus.svg" alt="add"/>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
